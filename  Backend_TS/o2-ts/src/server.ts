30a6e7e7860a2acaaf944b2911ec4514aa71b3b9
Sai kiran <saikiran29.u@gmail.com>
2026-03-15 13:27:09 +0530

    creating server using node, ts and making it run on express

---
+++  Backend_TS/o2-ts/src/server.ts
@@ -0,0 +1,19 @@
+ import http from "node:http";
+ import { env } from "./port.js";
+ import {createServer } from "./app/ex.js"
+ 
+ export async function main() {
+   try {
+     const server = http.createServer(createServer());
+     const PORT: number = env.PORT ? +env.PORT : 5000;
+ 
+     server.listen(PORT ,
+       () => {
+         console.log(`server is running on ${PORT} port..`);
+       });
+   } catch (error) {
+     throw error;
+   }
+ }
+ 
+ main();
