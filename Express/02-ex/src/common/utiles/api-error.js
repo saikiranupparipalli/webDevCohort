0d849938b5e4d2cecea04b52a82bbbdc3a320ea2
Sai kiran <saikiran29.u@gmail.com>
2026-04-02 03:18:01 +0530

    working of backend layered architecture

--- Express/02-ex/src/common/utiles/api-error.js
+++ Express/02-ex/src/common/utiles/api-error.js
@@ -1,20 +1,21 @@
 class ApiError extends Error {
+  
   constructor(statusCode, message) {
     super(message);
     this.statusCode = statusCode;
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
-  
+
   static badRequest(message = "Bad request") {
-    return new ApiError(404, message)
+    return new ApiError(404, message);
   }
-  
+
   static unauthorized(message = "unauthorized") {
-    return new ApiError(404, message)
+    return new ApiError(404, message);
   }
   static conflict(message = "conflict- User already exist") {
-    return new ApiError(409, message)
+    return new ApiError(409, message);
   }
 }
-export default ApiError
+export default ApiError;
--- Express/02-ex/src/modules/auth/auth.routes.js
+++ Express/02-ex/src/modules/auth/auth.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express"
-import  * as controller from "./auth.controller.js"
+import * as controller from "./auth.controller.js"
 import validate from "../../common/middleware/validate-middleware.js"
 import RegisterDto from "./dto/register.dto.js"
 
 const router = Router()
 
-router.post("/register", validate(RegisterDto), controller.register)
+router.post("/register", validate(RegisterDto), controller.default)
 
 export { router } 
