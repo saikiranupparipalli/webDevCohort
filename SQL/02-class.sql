6f9ea3808e3815cb9af829f9add1d98810ad6bda
Sai kiran <saikiran29.u@gmail.com>
2026-06-04 05:21:51 +0530

    sql prep

--- SQL/02-class.sql
+++ SQL/02-class.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ip_players(
     player_id SERIAL PRIMARY KEY,
     player_name VARCHAR(50) NOT NULL,
-    runs VARCHAR(50) NOT NULL,
+    runs INT,
     run_scored INT CHECK(run_scored > 0)
 );
 
@@ -14,24 +14,48 @@ ADD COLUMN team_name VARCHAR(50),
 ADD COLUMN role VARCHAR(50),
 ADD COLUMN matches_played INT;
 
+
 INSERT INTO ip_players (player_name, team_name, role, matches_played, runs, run_scored) VALUES
-('Virat Kohli', 'RCB', 'Batsman', 15, 'Excellent', 973),
-('Rohit Sharma', 'MI', 'Batsman', 16, 'Good', 890),
-('MS Dhoni', 'CSK', 'Wicket Keeper', 14, 'Average', 520),
-('Shubman Gill', 'GT', 'Batsman', 17, 'Excellent', 950),
-('Hardik Pandya', 'MI', 'All Rounder', 15, 'Good', 680),
-('KL Rahul', 'LSG', 'Batsman', 14, 'Good', 720),
-('Ruturaj Gaikwad', 'CSK', 'Batsman', 16, 'Excellent', 810),
-('Jasprit Bumrah', 'MI', 'Bowler', 15, 'Average', 120),
-('Mohammed Siraj', 'RCB', 'Bowler', 14, 'Average', 90),
-('Suryakumar Yadav', 'MI', 'Batsman', 15, 'Excellent', 840),
-('Ravindra Jadeja', 'CSK', 'All Rounder', 16, 'Good', 460),
-('Yashasvi Jaiswal', 'RR', 'Batsman', 14, 'Excellent', 780),
-('Sanju Samson', 'RR', 'Wicket Keeper', 15, 'Good', 650),
-('Andre Russell', 'KKR', 'All Rounder', 13, 'Good', 540),
-('Sunil Narine', 'KKR', 'All Rounder', 15, 'Good', 500);
+('Virat Kohli', 'RCB', 'Batsman', 15 , 5543, 973),
+('Rohit Sharma', 'MI', 'Batsman', 16,423, 890),
+('MS Dhoni', 'CSK', 'Wicket Keeper', 14,694, 520),
+('Shubman Gill', 'GT', 'Batsman', 17, 950, 890),
+('Hardik Pandya', 'MI', 'All Rounder', 15, 680, 732),
+('KL Rahul', 'LSG', 'Batsman', 14, 720, 123),
+('Ruturaj Gaikwad', 'CSK', 'Batsman', 16, 810, 1000),
+('Jasprit Bumrah', 'MI', 'Bowler', 15, 120, 127),
+('Mohammed Siraj', 'RCB', 'Bowler', 14, 90, 658),
+('Suryakumar Yadav', 'MI', 'Batsman', 15, 840, 331),
+('Ravindra Jadeja', 'CSK', 'All Rounder', 16, 460, 829),
+('Yashasvi Jaiswal', 'RR', 'Batsman', 14, 780, 1456),
+('Sanju Samson', 'RR', 'Wicket Keeper', 15, 650, 231),
+('Andre Russell', 'KKR', 'All Rounder', 13, 540, 106),
+('Sunil Narine', 'KKR', 'All Rounder', 15, 500, 798);
 
 -- SELECT * FROM ip_players;
 
-SELECT team_name, player_name FROM ip_players WHERE run_scored > '650'
+-- SELECT team_name, player_name FROM ip_players WHERE run_scored > '650';
+ 
+-- SELECT player_name FROM ip_players WHERE runs > '15' AND run_scored >  '800';
+
+-- SELECT player_name FROM ip_players WHERE team_name = 'CSK' OR team_name = 'MI';
+
+-- SELECT player_name FROM ip_players WHERE player_name LIKE '__s_%'
+
+-- SELECT * FROM ip_players WHERE team_name IN('RCB', 'RR');
+
+SELECT * FROM ip_players WHERE run_scored BETWEEN 700 AND 810 ;
+
+SELECT * FROM ip_players WHERE team_name != 'KKR';
+
+SELECT * FROM ip_players ORDER BY player_name ASC;
+
+SELECT player_name, role FROM ip_players ORDER BY player_name DESC;
+
+SELECT player_name, run_scored FROM ip_players ORDER BY player_name ASC, run_scored DESC;
+
+SELECT * FROM ip_players ORDER BY player_name LIMIT 10;
+
+ 
+
  
---
+++ SQL/03-prep.sql
@@ -0,0 +1,31 @@
+CREATE TABLE ipl_player(
+    player_id SERIAL PRIMARY KEY,
+    player_name VARCHAR(50) NOT NULL,
+    team_name VARCHAR(50),
+    role VARCHAR(20),
+    matches_played INT,
+    runs INT,
+    run_scored INT
+);
+
+INSERT INTO ipl_player(player_name, team_name, role, matches_played, runs, run_scored) VALUES
+('Virat Kohli', 'RCB', 'Batsman', 15 , 5543, 973),
+('Rohit Sharma', 'MI', 'Batsman', 16,423, 890),
+('MS Dhoni', 'CSK', 'Wicket Keeper', 14,694, 520),
+('Shubman Gill', 'GT', 'Batsman', 17, 950, 890),
+('Hardik Pandya', 'MI', 'All Rounder', 15, 680, 732),
+('KL Rahul', 'LSG', 'Batsman', 14, 720, 123),
+('Ruturaj Gaikwad', 'CSK', 'Batsman', 16, 810, 1000),
+('Jasprit Bumrah', 'MI', 'Bowler', 15, 120, 127),
+('Mohammed Siraj', 'RCB', 'Bowler', 14, 90, 658),
+('Suryakumar Yadav', 'MI', 'Batsman', 15, 840, 331),
+('Ravindra Jadeja', 'CSK', 'All Rounder', 16, 460, 829),
+('Yashasvi Jaiswal', 'RR', 'Batsman', 14, 780, 1456),
+('Sanju Samson', 'RR', 'Wicket Keeper', 15, 650, 231),
+('Andre Russell', 'KKR', 'All Rounder', 13, 540, 106),
+('Sunil Narine', 'KKR', 'All Rounder', 15, 500, 798);
+
+SELECT * FROM ipl_player;
+
+SELECT * FROM ipl_player ORDER BY player_name DESC, runs > '600' DESC LIMIT 3
+
