3e2c0602fae1c1bf4b39210aa73f82acf289668d
Sai kiran <saikiran29.u@gmail.com>
2026-06-07 04:07:56 +0530

    transactions done!

--- SQL/transactions/01-transaction.sql
+++ SQL/transactions/01-transaction.sql
@@ -10,3 +10,22 @@ VALUES
 ('charan', 5000);
 
 SELECT * FROM transactions;
+
+-- BEGIN
+-- UPDATE transactions SET balance = balance - 500 WHERE owner = 'sai kiran';
+-- SELECT * FROM transactions;
+-- UPDATE transactions SET balance = balance + 500 WHERE owner = 'sai kiran';
+-- SELECT * FROM transactions;
+-- COMMIT 
+-- ROLLBACK
+
+
+
+-- docker postgres(ps)
+
+-- docker run -d --name <container name> -e POSTGRES_PASSWORD=<password> -p <ports> postgres:<database version>
+
+-- docker exec -it <docker container name> -U <username>
+-- \l -to the list to db's in vs code
+-- \c -your database_name -to connect to db
+-- \dt -to see the tables inside db
