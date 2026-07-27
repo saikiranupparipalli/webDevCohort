079dac5c7022ceb55e59cff5a9f14bd84add252d
Sai kiran <saikiran29.u@gmail.com>
2026-07-28 01:20:19 +0530

    react intermediate

--- index.js
+++ index.js
@@ -8,3 +8,4 @@
  console.log(typeof convert);
 
 
+ const arr = [1, 2, 3, 4, 5];
--- react/intermediate-05/src/App.jsx
+++ react/intermediate-05/src/App.jsx
@@ -1,25 +1,26 @@
 import { useEffect, useState } from "react";
-
 import "./App.css";
-
+import { ChaiMenu } from "./ChaiMenu.jsx";
 function App() {
   const [data, setData] = useState(0);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/all-chai`)
-    .then((response)=> response.json())
-    .then((data)=> {setData(data)
-     return data
-  })
-    .then((data)=> console.log(data))
-    .catch((error)=> console.log('Error fetching data',error))
-  }, [])
-
-  return <>
-  <h1>Hello, from react</h1>
-  <p>Data from API: {data ? JSON.stringify(data): "loading.."}
-  </p>
-  </>;
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        return data;
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.log("Error fetching data", error));
+  }, [ ]);
+  return (
+    <>
+      <h1>Hello, from react</h1>
+      <p>Data from API: {data ? JSON.stringify(data) : "loading..🔃"}</p>
+      <ChaiMenu/>
+    </>
+  );
 }
 
 export default App;
